cd1a585476f2a9d3a3521ef40f3fc0398567dabc
Abdurahim <abdurahimsharipov086@gmaile.com>
2025-07-02 16:10:39 +0500

    LoginScreen

---
+++ composeApp/src/commonMain/kotlin/org/example/project/presentation/base/CustomText.kt
@@ -0,0 +1,91 @@
+package uz.saidburxon.newedu.presentation.base
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
+import org.example.project.TextColor
+import org.jetbrains.compose.ui.tooling.preview.Preview
+@Composable
+fun CustomText(
+    text: AnnotatedString,
+    modifier: Modifier = Modifier,
+    style: TextStyle = MaterialTheme.typography.titleMedium,
+    color: Color = TextColor,
+    maxLines: Int = Int.MAX_VALUE,
+    minLines: Int = 1,
+    fontWeight: FontWeight = FontWeight.Normal,
+    fontSize:TextUnit = TextUnit.Unspecified,
+    softWrap: Boolean = true,
+    lineHeight:TextUnit = TextUnit.Unspecified,
+    letterSpacing: TextUnit = TextUnit.Unspecified,
+    fontStyle: FontStyle = FontStyle.Normal
+
+) {
+    Text(
+       text = text,
+        modifier = modifier,
+        style = style,
+        color = color,
+        maxLines = maxLines,
+        minLines = minLines,
+        fontWeight = fontWeight,
+        fontSize = fontSize,
+        softWrap = softWrap,
+        lineHeight = lineHeight,
+        letterSpacing = letterSpacing,
+        fontFamily = FontFamily(),
+        fontStyle = fontStyle
+    )
+}
+
+
+@Composable
+fun CustomText(
+    text: String,
+    modifier: Modifier = Modifier,
+    style: TextStyle = MaterialTheme.typography.titleMedium,
+    color: Color = TextColor,
+    maxLines: Int = Int.MAX_VALUE,
+    minLines: Int = 1,
+    fontWeight: FontWeight = FontWeight.Normal,
+    fontSize:TextUnit = TextUnit.Unspecified,
+    softWrap: Boolean = true,
+    lineHeight:TextUnit = TextUnit.Unspecified,
+    letterSpacing: TextUnit = TextUnit.Unspecified,
+    fontStyle: FontStyle = FontStyle.Normal,
+    textAlign: TextAlign = TextAlign.Start
+) {
+    Text(
+        textAlign = textAlign,
+        text = text,
+        modifier = modifier,
+        style = style,
+        color = color,
+        maxLines = maxLines,
+        minLines = minLines,
+        fontWeight = fontWeight,
+        fontSize = fontSize,
+        softWrap = softWrap,
+        lineHeight = lineHeight,
+        letterSpacing = letterSpacing,
+        fontFamily = FontFamily(),
+        fontStyle = fontStyle
+    )
+}
+
+
+
+@Preview
+@Composable
+private fun Pre() {
+
+}
---
+++ composeApp/src/commonMain/kotlin/org/example/project/presentation/base/LogoText.kt
@@ -0,0 +1,56 @@
+package org.example.project.presentation.base
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import uz.saidburxon.newedu.presentation.base.CustomText
+
+@Composable
+fun LogoText(
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(start = 24.dp, end = 24.dp, top = 5.dp, bottom = 24.dp)
+    ) {
+        CustomText(
+            text = getStyledText(),
+            fontSize = 40.sp,
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.Bold,
+        )
+    }
+}
+
+
+
+fun getStyledText(): AnnotatedString {
+    return buildAnnotatedString {
+        withStyle(style = SpanStyle(color = Color.Black)) {
+            append("New ")
+        }
+        withStyle(style = SpanStyle(color = Color.White)) {
+            append("edu")
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun Pre() {
+    LogoText()
+}
