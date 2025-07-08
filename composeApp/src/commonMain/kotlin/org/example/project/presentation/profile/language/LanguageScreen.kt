7d3a0078b2096141269363d5147d9330aea1188d
Azizbek <muminjonovazizbek0@gmail.com>
2025-07-08 17:27:43 +0500

    Profile, Personal Information and Language Screens are done

--- composeApp/src/commonMain/kotlin/org/example/project/presentation/profile/language/LanguageScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/presentation/profile/language/LanguageScreen.kt
@@ -2,7 +2,9 @@ package org.example.project.presentation.profile.language
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -13,10 +15,12 @@ import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.Navigator
+import org.example.project.presentation.common.CustomButton
 import org.example.project.presentation.domain.model.LanguageType
 import org.example.project.presentation.profile.CustomHeader
 import org.example.project.ui.BackgroundColor
 import org.example.project.ui.ContainerPadding
+import org.example.project.ui.NormalLargeTextSize
 import org.example.project.ui.SpaceMedium
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -66,6 +70,22 @@ fun LanguageUi(
             LanguageSelection(
                 selectedLanguage = selectedLanguage,
                 onLanguageSelected = {selectedLanguage = it}
+            )
+
+            Spacer(
+                modifier = Modifier
+                    .weight(1f)
+            )
+
+            CustomButton(
+                text = "Davom etish",
+                enabled = true,
+                modifier = Modifier
+                    .fillMaxWidth(),
+                onClick = {
+                    navigator!!.pop()
+                },
+                fontSize = NormalLargeTextSize
             )
 
         }
