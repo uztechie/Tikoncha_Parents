28aba4e21ada26822ee52925c127cba52df29f11
Ibroxim Odilov <ibroxim@chowis.com>
2025-07-12 14:41:36 +0500

    Some fixes

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,9 +1,14 @@
 import SwiftUI
+import ComposeApp
+import YandexMapsMobile
 
 @main
 struct iOSApp: App {
-    
-    
+
+    init() {
+        AppKt.doInitMapKit()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
