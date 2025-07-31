2117692d8cbe20b9d815ff8ed54567041216386f
Abdurahim <abdurahimsharipov086@gmaile.com>
2025-07-31 17:05:50 +0500

    DarkMode

--- composeApp/src/androidMain/kotlin/org/example/project/ui/theme/PlatformThemeBridge.android.kt
+++ composeApp/src/androidMain/kotlin/org/example/project/ui/theme/PlatformThemeBridge.android.kt
@@ -1,9 +1,20 @@
 package org.example.project.ui.theme
 
+import androidx.appcompat.app.AppCompatDelegate
+
 actual object   PlatformThemeBridge {
-    actual fun onModeChanged(mode: ThemeMode) {
-    }
 
     actual fun applyInitial(mode: ThemeMode) {
+        onModeChanged(mode)
+    }
+
+    actual fun onModeChanged(mode: ThemeMode) {
+        val nightMode = when (mode) {
+            ThemeMode.SYSTEM -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+            ThemeMode.LIGHT  -> AppCompatDelegate.MODE_NIGHT_NO
+            ThemeMode.DARK   -> AppCompatDelegate.MODE_NIGHT_YES
+        }
+        println("PlatformThemeBridge onModeChanged=$mode")
+        AppCompatDelegate.setDefaultNightMode(nightMode)
     }
 }
