75a27e5ff2d4263b36bc05e29b42fde4c3a7bf6b
Ibroxim Odilov <ibroxim@chowis.com>
2025-08-04 16:56:27 +0500

    fix

--- composeApp/src/iosMain/kotlin/org/example/project/platform/SystemBarTheme.ios.kt
+++ composeApp/src/iosMain/kotlin/org/example/project/platform/SystemBarTheme.ios.kt
@@ -3,7 +3,7 @@ package org.example.project.platform
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-actual class SystemBarTheme {
+actual object SystemBarTheme {
     @Composable
     actual fun apply(
         isDark: Boolean,
