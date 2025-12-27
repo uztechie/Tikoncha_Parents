0c17fd5326732d89c60196ac085252d9a04bb2d1
Abdurahim <abdurahimsharipov086@gmaile.com>
2025-12-27 10:44:55 +0500

    bugs_fix 25.12.2025

--- composeApp/src/commonMain/kotlin/uz/tikoncha_parent/presentation/task/TaskViewModel.kt
+++ composeApp/src/commonMain/kotlin/uz/tikoncha_parent/presentation/task/TaskViewModel.kt
@@ -239,7 +239,7 @@ class TaskViewModel (
                 target_user_id = selectedChildUserId,
                 id = Uuid.random().toString(),
                 is_completed = _state.value.completed,
-                coins = _state.value.coin
+                coin = _state.value.coin
             )
 
             val result = todoUseCase(request)
