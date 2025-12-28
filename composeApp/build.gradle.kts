4f861fdb906f15b25acc7bdc0a7d11f28d0f5e59
Ibroxim Odilov <ibroxim@chowis.com>
2025-12-28 15:44:41 +0500

    Bug fixes

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -176,8 +176,8 @@ android {
         applicationId = "uz.tikoncha.parent"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 3
-        versionName = "1.0.3"
+        versionCode = 4
+        versionName = "1.0.4"
     }
     packaging {
         resources {
