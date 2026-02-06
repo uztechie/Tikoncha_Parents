3276acf05f06d99ac34c30db6c2a1638fae27d65
Ibroxim Odilov <ibroxim@chowis.com>
2026-02-06 18:33:43 +0500

    v1.0.6 is released

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -177,8 +177,8 @@ android {
         applicationId = "uz.tikoncha.parent"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 5
-        versionName = "1.0.5"
+        versionCode = 6
+        versionName = "1.0.6"
     }
     packaging {
         resources {
